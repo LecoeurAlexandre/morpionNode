911b571c8c5bffeaa033409f081fa0e2f9c3f8bc
Alexandre Lecoeur <alexandre.lecoeur@gmail.com>
2023-03-07 17:34:17 +0100

    display table ok

--- classe/ihm.js
+++ classe/ihm.js
@@ -2,12 +2,20 @@ import { poserUneQuestion } from "../tools/tools.js";
 
 export class Ihm {
     constructor() {
+        this.table = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
         this.coordinates = []
         this.whichPlayer = true
         this.counter = 0
     }
 
     async instructions() {
+            for(let i=0; i<this.table.length; i++) {
+                let ligne = ''
+                for(let j=0; j<this.table[i].length; j++){
+                    ligne += '|'+this.table[i][j]+'|'
+                }
+                console.log(ligne)
+            }
             do {
                 console.log("Bienvenue au jeu du morpion")
                 if (this.whichPlayer) {
@@ -17,6 +25,7 @@ export class Ihm {
                 }
                 const x = await poserUneQuestion("Veuillez renseigner l'axe horizontal")
                 const y = await poserUneQuestion("Veuillez renseigner l'axe vertical")
+                if (x < 3 || y < 3) {} 
                 this.coordinates.push({
                     x: x,
                     y: y
